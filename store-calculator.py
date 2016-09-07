505546dadd8669a31ba93bebf6566ccdce6f8889
Petronella <petronella.turesson@gmail.com>
2016-09-07 16:21:03 +0200

    Created a price calculator for my robotshop

--- store-calculator.py
+++ store-calculator.py
@@ -1,1 +1,19 @@
-print(900 * 2 * 1.25 + 100 * 1 * 1.06)
+class Product:
+	price = 0
+	count = 0
+	tax = 1
+	def __init__(self, price, count, tax):
+		self.price = price
+		self.count = count
+		self.tax = tax
+
+	def price_with_tax(self):
+		return self.price * self.count * self.tax
+
+products = [Product(price=900, count=2, tax=1.25), Product(price=100, count=1, tax=1.06)]
+total_price = 0
+
+for product in products:
+	total_price += product.price_with_tax()
+
+print(total_price)
